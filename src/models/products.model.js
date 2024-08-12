0520eec6e896f9ecdde2e36e224100cecb66193d
Adeyemi Adedapo <adedapo025@gmail.com>
2024-08-12 21:13:41 +0100

    Created products DB model

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -1,10 +1,12 @@
-const momgoose = require('mongoose')
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
+    name: {type: String, required: true, min: 2, unique: true},
     category: String,
-    price: {type: String, required: true},
-    stock: {type: Number, required: String},
+    price: {type: Number, required: true},
+    stock: {type: Number, required: true, default: 1},
+    availability: {type: Boolean, default: true},
     description: String
 })
+
+module.exports = mongoose.model('Product', productSchema)
