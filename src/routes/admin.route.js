f429a09ae6484b4ebacb0e1cb4eb315f278b5ad4
Adeyemi Adedapo <adedapo025@gmail.com>
2024-08-12 21:12:52 +0100

    Added routes to admin functionalities

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -4,12 +4,18 @@ const verifyJWT = require("../middlewares/verifyJWT")
 const verifyAdminStatus = require('../middlewares/verifyAdminStatus')
 const getAllUsers = require('../controllers/admin.controller')
 const httpLogin = require('../controllers/auth.controller')
+const { addWears } = require('../controllers/products.controller')
+
+adminRouter.post('/auth/login', httpLogin)
 
 adminRouter.use(verifyJWT)
 adminRouter.use(verifyAdminStatus)
 
-adminRouter.post('/admin/auth/login', httpLogin)
+
 adminRouter.get('/get-users', getAllUsers)
+adminRouter.post('/add-wears', addWears)
+adminRouter.get('/show-all-wears')
+
 
 
 module.exports = adminRouter
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,9 +2,11 @@ const express = require("express")
 const userRouter = express.Router()
 const httpRegister = require('../controllers/user.controller')
 const httpLogin = require('../controllers/auth.controller')
+const verifyJWT = require('../middlewares/verifyJWT')
+const { getWears } = require('../controllers/products.controller')
 
-const { body } = require("express-validator")
-const checkErrors = [
+const { body, query } = require("express-validator")
+const checkInputErrors = [
     body('firstName').notEmpty().trim(),
     body('lastName').notEmpty().trim(),
     body('email').isEmail(),
@@ -13,8 +15,20 @@ const checkErrors = [
         return true
     })]
 
-userRouter.post('/register', checkErrors, httpRegister)
+const checkQueryError = [
+    query('category').optional().isString().trim(),
+    query('minPrice').optional().isInt({min: 0}),
+    query('maxPrice').optional().isInt({min: 0}),
+    query('available').optional().isBoolean(),
+    query('sort').optional().isIn(['price_asc', 'price_desc'])
+]
+
+userRouter.post('/register', checkInputErrors, httpRegister)
 userRouter.post('/auth/login', httpLogin)
-userRouter.post('/', (req, res) => res.send("Heyy there"))
+
+userRouter.use(verifyJWT)
+
+userRouter.get('get-wears', checkQueryError, getWears)
+
 
 module.exports = userRouter
