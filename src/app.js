2595190b3ba26e0bcb2df93c28290934373ad77f
Adeyemi Adedapo <adedapo025@gmail.com>
2024-08-12 21:14:44 +0100

    Redefined Router path for admin

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
-const verifyJWT = require('./middlewares/verifyJWT')
-const userRoutes = require('./routes/user.route')
+const userRouter = require('./routes/user.route')
+const adminRouter = require('./routes/admin.route')
 
 const app = express()
 
 // middlewares
 app.use(express.json())
-app.use(verifyJWT)
-
 
 // routes
+app.use("/admin", adminRouter)
+app.use(userRouter)
+
+app.use('/*', (req, res) =>{
+    return res.status(404).json({message: "Page Not Found!"})
+})
 
 
 
