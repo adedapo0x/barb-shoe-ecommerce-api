4f1346e550aa6fa6aaca3b66e7f5b54f4160ed16
Adeyemi Adedapo <adedapo025@gmail.com>
2024-08-20 12:22:34 +0100

    created startServer function cos of asynchronous nature of connectDB

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require('./src/config/connectDB')
 const PORT = 8000 || process.env.PORT
-const server = http.createServer(app)
 
-connectDB()
+const startServer = async (req, res) => {
+    const server = http.createServer(app)
+    await connectDB()
 
-server.listen(PORT, ()=> console.log("Server running fine!"))
+    server.listen(PORT, ()=> console.log("Server running fine!"))
+}
+
+startServer()
+
+
+
